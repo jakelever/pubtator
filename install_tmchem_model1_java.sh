d6c66ddd51f7bf76946939acba6ea45487f0906c
Jake Lever <jlever@bcgsc.ca>
2019-05-03 17:03:11 -0700

    Change to patch directory

--- install_tmchem_model1_java.sh
+++ install_tmchem_model1_java.sh
@@ -5,20 +5,16 @@ rm -fr Ab3P-v1.5
 rm -f tmChemM1-0.0.2.tgz
 rm -f Ab3P-v1.5.tar.gz
 
-#cp ../PGmine/tmChemM1-0.0.2/src/ncbi/chemdner/AbbreviationIdentifier.java tmChemUpdates
-#cp ../PGmine/tmChemM1-0.0.2/src/ncbi/Run.java tmChemUpdates
-
-
 wget https://www.ncbi.nlm.nih.gov/CBBresearch/Lu/Demo/tmTools/download/tmChem/tmChemM1-0.0.2.tgz
 tar xvf tmChemM1-0.0.2.tgz 
 cd tmChemM1-0.0.2
 
-cp ../tmChemUpdates/Run.java ./src/ncbi/Run.java
-cp ../tmChemUpdates/AbbreviationIdentifier.java ./src/ncbi/chemdner/AbbreviationIdentifier.java
-cp ../tmChemUpdates/MultiTokenRegexMatcher.java ./src/ncbi/chemdner/MultiTokenRegexMatcher.java
+cp ../patches/tmChemM1-0.0.2/Run.java ./src/ncbi/Run.java
+cp ../patches/tmChemM1-0.0.2/AbbreviationIdentifier.java ./src/ncbi/chemdner/AbbreviationIdentifier.java
+cp ../patches/tmChemM1-0.0.2/MultiTokenRegexMatcher.java ./src/ncbi/chemdner/MultiTokenRegexMatcher.java
 
-cp ../tmChemUpdates/build.xml ./build.xml
-cp ../tmChemUpdates/Run.sh ./Run.sh
+cp ../patches/tmChemM1-0.0.2/build.xml ./build.xml
+cp ../patches/tmChemM1-0.0.2/Run.sh ./Run.sh
 
 rm -fr bin
 ant build.project
